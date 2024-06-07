add2195fbc875bc7c0fb24dcf710431a2472bd0b
lucasberenger <thegreatberenger@gmail.com>
2024-06-07 19:58:24 -0300

    first commit and first exercise completed.

---
+++ src/curso_programcao/Exercicio1.java
@@ -0,0 +1,18 @@
+package curso_programcao;
+import java.util.Scanner;
+
+public class Exercicio1 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Insira um valor inteiro:");
+		int n1 = sc.nextInt();
+		
+		System.out.println("Insira outro valor inteiro:");
+		int n2 = sc.nextInt();
+		
+		int soma = n1 + n2;
+		System.out.println("SOMA: " + soma);
+	}
+}
